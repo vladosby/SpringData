37a279354045418d8453069e80fcb6b65c91a3b7
vladosby <vladislavn.kievski@gmail.com>
2016-09-07 14:25:03 +0300

    Refactoring JavaConfig, adding PostgreConfig.  Adding property file with database settings.

--- src/main/java/com/spring/data/config/PostgreJavaConfig.java
+++ src/main/java/com/spring/data/config/PostgreJavaConfig.java
@@ -2,9 +2,7 @@ package com.spring.data.config;
 
 import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
